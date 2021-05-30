ef2a1b174eb1e400921d083bbecab01d7db5a44c
rohith-nagothu <84123437+rohith-nagothu@users.noreply.github.com>
2021-05-30 10:48:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function data() {
+	window.alert("Dhoni was born in Ranchi, Bihar (now in Jharkhand), and he hails from a Hindu Rajput family. His paternal village Lwali is in the Lamgara block of the Almora District of Uttarakhand. Dhoni's parents moved from Uttarakhand to Ranchi, where his father Pan Singh worked in junior management positions in MECON. Dhoni has a sister Jayanti Gupta and a brother Narendra Singh Dhoni.Batting: Right-handed")
+}
